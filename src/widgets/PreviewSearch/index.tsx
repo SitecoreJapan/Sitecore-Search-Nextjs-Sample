9c9391b21d9ebafa0250c9aa2e797df437d26fe0
Shinichi Haramizu <shinichi.haramizu@sitecore.com>
2023-09-16 18:59:52 +0900

    add next/router
    previewsearch don't include page navigation.

--- src/widgets/PreviewSearch/index.tsx
+++ src/widgets/PreviewSearch/index.tsx
@@ -8,6 +8,7 @@ import {
 } from "@sitecore-search/react";
 import { ArticleCard, NavMenu, Presence } from "@sitecore-search/ui";
 import type { PreviewSearchActionProps } from "@sitecore-search/widgets";
+import { useRouter } from "next/router";
 
 import styles from "./styles.module.css";
 
@@ -18,6 +19,7 @@ type ArticleModel = {
   url: string;
   source_id?: string;
 };
+
 const Articles = ({
   loading = false,
   articles,
@@ -26,59 +28,65 @@ const Articles = ({
   loading?: boolean;
   articles: Array<ArticleModel>;
   onItemClick: PreviewSearchActionProps["onItemClick"];
-}) => (
-  <NavMenu.Content className={styles["sitecore-nav-menu-grid"]}>
-    <Presence present={loading}>
-      <div className={styles["sitecore-loader-container"]}>
-        <svg
-          aria-busy={loading}
-          aria-hidden={!loading}
-          focusable="false"
-          role="progressbar"
-          viewBox="0 0 20 20"
-          className={styles["sitecore-loader-animation"]}
-        >
-          <path d="M7.229 1.173a9.25 9.25 0 1 0 11.655 11.412 1.25 1.25 0 1 0-2.4-.698 6.75 6.75 0 1 1-8.506-8.329 1.25 1.25 0 1 0-.75-2.385z" />
-        </svg>
-      </div>
-    </Presence>
-    <NavMenu.List className={styles["sitecore-nav-menu-sub-list"]}>
-      {!loading &&
-        articles.map((article, index) => (
-          <NavMenu.Item
-            key={article.id}
-            className={styles["sitecore-nav-menu-sub-item"]}
+}) => {
+  const router = useRouter();
+  return (
+    <NavMenu.Content className={styles["sitecore-nav-menu-grid"]}>
+      <Presence present={loading}>
+        <div className={styles["sitecore-loader-container"]}>
+          <svg
+            aria-busy={loading}
+            aria-hidden={!loading}
+            focusable="false"
+            role="progressbar"
+            viewBox="0 0 20 20"
+            className={styles["sitecore-loader-animation"]}
           >
-            <NavMenu.Link
-              href={article.url}
-              onClick={(e) => {
-                e.preventDefault();
-                onItemClick({
-                  id: article.id,
-                  index,
-                  sourceId: article.source_id,
-                });
-                // add redirection or any action
-              }}
-              className={styles["sitecore-nav-menu-link"]}
+            <path d="M7.229 1.173a9.25 9.25 0 1 0 11.655 11.412 1.25 1.25 0 1 0-2.4-.698 6.75 6.75 0 1 1-8.506-8.329 1.25 1.25 0 1 0-.75-2.385z" />
+          </svg>
+        </div>
+      </Presence>
+      <NavMenu.List className={styles["sitecore-nav-menu-sub-list"]}>
+        {!loading &&
+          articles.map((article, index) => (
+            <NavMenu.Item
+              key={article.id}
+              className={styles["sitecore-nav-menu-sub-item"]}
             >
-              <ArticleCard.Root className={styles["sitecore-article-root"]}>
-                <div className={styles["sitecore-article-image-wrapper"]}>
-                  <ArticleCard.Image
-                    src={article.image_url}
-                    className={styles["sitecore-article-image"]}
-                  />
-                </div>
-                <ArticleCard.Title className={styles["sitecore-article-name"]}>
-                  {article.name}
-                </ArticleCard.Title>
-              </ArticleCard.Root>
-            </NavMenu.Link>
-          </NavMenu.Item>
-        ))}
-    </NavMenu.List>
-  </NavMenu.Content>
-);
+              <NavMenu.Link
+                href={article.url}
+                onClick={(e) => {
+                  e.preventDefault();
+                  onItemClick({
+                    id: article.id,
+                    index,
+                    sourceId: article.source_id,
+                  });
+                  router.push(article.url);
+                  // add redirection or any action
+                }}
+                className={styles["sitecore-nav-menu-link"]}
+              >
+                <ArticleCard.Root className={styles["sitecore-article-root"]}>
+                  <div className={styles["sitecore-article-image-wrapper"]}>
+                    <ArticleCard.Image
+                      src={article.image_url}
+                      className={styles["sitecore-article-image"]}
+                    />
+                  </div>
+                  <ArticleCard.Title
+                    className={styles["sitecore-article-name"]}
+                  >
+                    {article.name}
+                  </ArticleCard.Title>
+                </ArticleCard.Root>
+              </NavMenu.Link>
+            </NavMenu.Item>
+          ))}
+      </NavMenu.List>
+    </NavMenu.Content>
+  );
+};
 type InitialState = PreviewSearchInitialState<"itemsPerPage">;
 export const PreviewSearchComponent = ({ defaultItemsPerPage = 6 }) => {
   const {
@@ -104,6 +112,7 @@ export const PreviewSearchComponent = ({ defaultItemsPerPage = 6 }) => {
     },
     [onKeyphraseChange]
   );
+
   return (
     <NavMenu.Root className={styles["sitecore-nav-menu-root"]}>
       <NavMenu.List className={styles["sitecore-nav-menu-main-list"]}>
