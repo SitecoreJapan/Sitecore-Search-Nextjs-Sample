bf2994c6ddd250975f4a4bd55c9875f270a0798d
Shinichi Haramizu <shinichi.haramizu@sitecore.com>
2023-09-16 18:47:11 +0900

    Update index.tsx

--- src/widgets/PreviewSearch/index.tsx
+++ src/widgets/PreviewSearch/index.tsx
@@ -1,11 +1,15 @@
-import { useCallback } from 'react';
+import { useCallback } from "react";
 
-import type { PreviewSearchInitialState } from '@sitecore-search/react';
-import { WidgetDataType, usePreviewSearch, widget } from '@sitecore-search/react';
-import { ArticleCard, NavMenu, Presence } from '@sitecore-search/ui';
-import type { PreviewSearchActionProps } from '@sitecore-search/widgets';
+import type { PreviewSearchInitialState } from "@sitecore-search/react";
+import {
+  WidgetDataType,
+  usePreviewSearch,
+  widget,
+} from "@sitecore-search/react";
+import { ArticleCard, NavMenu, Presence } from "@sitecore-search/ui";
+import type { PreviewSearchActionProps } from "@sitecore-search/widgets";
 
-import styles from './styles.module.css';
+import styles from "./styles.module.css";
 
 type ArticleModel = {
   id: string;
@@ -21,27 +25,30 @@ const Articles = ({
 }: {
   loading?: boolean;
   articles: Array<ArticleModel>;
-  onItemClick: PreviewSearchActionProps['onItemClick'];
+  onItemClick: PreviewSearchActionProps["onItemClick"];
 }) => (
-  <NavMenu.Content className={styles['sitecore-nav-menu-grid']}>
+  <NavMenu.Content className={styles["sitecore-nav-menu-grid"]}>
     <Presence present={loading}>
-      <div className={styles['sitecore-loader-container']}>
+      <div className={styles["sitecore-loader-container"]}>
         <svg
           aria-busy={loading}
           aria-hidden={!loading}
           focusable="false"
           role="progressbar"
           viewBox="0 0 20 20"
-          className={styles['sitecore-loader-animation']}
+          className={styles["sitecore-loader-animation"]}
         >
           <path d="M7.229 1.173a9.25 9.25 0 1 0 11.655 11.412 1.25 1.25 0 1 0-2.4-.698 6.75 6.75 0 1 1-8.506-8.329 1.25 1.25 0 1 0-.75-2.385z" />
         </svg>
       </div>
     </Presence>
-    <NavMenu.List className={styles['sitecore-nav-menu-sub-list']}>
+    <NavMenu.List className={styles["sitecore-nav-menu-sub-list"]}>
       {!loading &&
         articles.map((article, index) => (
-          <NavMenu.Item key={article.id} className={styles['sitecore-nav-menu-sub-item']}>
+          <NavMenu.Item
+            key={article.id}
+            className={styles["sitecore-nav-menu-sub-item"]}
+          >
             <NavMenu.Link
               href={article.url}
               onClick={(e) => {
@@ -53,13 +60,18 @@ const Articles = ({
                 });
                 // add redirection or any action
               }}
-              className={styles['sitecore-nav-menu-link']}
+              className={styles["sitecore-nav-menu-link"]}
             >
-              <ArticleCard.Root className={styles['sitecore-article-root']}>
-                <div className={styles['sitecore-article-image-wrapper']}>
-                  <ArticleCard.Image src={article.image_url} className={styles['sitecore-article-image']} />
+              <ArticleCard.Root className={styles["sitecore-article-root"]}>
+                <div className={styles["sitecore-article-image-wrapper"]}>
+                  <ArticleCard.Image
+                    src={article.image_url}
+                    className={styles["sitecore-article-image"]}
+                  />
                 </div>
-                <ArticleCard.Title className={styles['sitecore-article-name']}>{article.name}</ArticleCard.Title>
+                <ArticleCard.Title className={styles["sitecore-article-name"]}>
+                  {article.name}
+                </ArticleCard.Title>
               </ArticleCard.Root>
             </NavMenu.Link>
           </NavMenu.Item>
@@ -67,12 +79,16 @@ const Articles = ({
     </NavMenu.List>
   </NavMenu.Content>
 );
-type InitialState = PreviewSearchInitialState<'itemsPerPage'>;
+type InitialState = PreviewSearchInitialState<"itemsPerPage">;
 export const PreviewSearchComponent = ({ defaultItemsPerPage = 6 }) => {
   const {
     widgetRef,
     actions: { onItemClick, onKeyphraseChange },
-    queryResult: { isFetching, isLoading, data: { content: articles = [] } = {} },
+    queryResult: {
+      isFetching,
+      isLoading,
+      data: { content: articles = [] } = {},
+    },
   } = usePreviewSearch<ArticleModel, InitialState>({
     state: {
       itemsPerPage: defaultItemsPerPage,
@@ -86,28 +102,28 @@ export const PreviewSearchComponent = ({ defaultItemsPerPage = 6 }) => {
         keyphrase: target.value,
       });
     },
-    [onKeyphraseChange],
+    [onKeyphraseChange]
   );
   return (
-    <NavMenu.Root className={styles['sitecore-nav-menu-root']}>
-      <NavMenu.List className={styles['sitecore-nav-menu-main-list']}>
-        <NavMenu.Item className={styles['sitecore-nav-menu-main-list-item']}>
+    <NavMenu.Root className={styles["sitecore-nav-menu-root"]}>
+      <NavMenu.List className={styles["sitecore-nav-menu-main-list"]}>
+        <NavMenu.Item className={styles["sitecore-nav-menu-main-list-item"]}>
           <NavMenu.InputTrigger
             onChange={keyphraseHandler}
             autoComplete="off"
             placeholder="Type to search..."
-            className={styles['sitecore-nav-menu-input-trigger']}
+            className={styles["sitecore-nav-menu-input-trigger"]}
           />
-          <NavMenu.Content className={styles['sitecore-nav-menu-main-content']}>
+          <NavMenu.Content className={styles["sitecore-nav-menu-main-content"]}>
             <Presence present={loading}>
-              <div className={styles['sitecore-loader-container']}>
+              <div className={styles["sitecore-loader-container"]}>
                 <svg
                   aria-busy={loading}
                   aria-hidden={!loading}
                   focusable="false"
                   role="progressbar"
                   viewBox="0 0 20 20"
-                  className={styles['sitecore-loader-animation']}
+                  className={styles["sitecore-loader-animation"]}
                 >
                   <path d="M7.229 1.173a9.25 9.25 0 1 0 11.655 11.412 1.25 1.25 0 1 0-2.4-.698 6.75 6.75 0 1 1-8.506-8.329 1.25 1.25 0 1 0-.75-2.385z" />
                 </svg>
@@ -118,17 +134,20 @@ export const PreviewSearchComponent = ({ defaultItemsPerPage = 6 }) => {
                 orientation="vertical"
                 value="defaultArticlesResults"
                 ref={widgetRef}
-                className={styles['sitecore-nav-menu-sub-content']}
+                className={styles["sitecore-nav-menu-sub-content"]}
               >
                 <NavMenu.List
-                  className={`${styles['sitecore-nav-menu-main-list']} ${styles['sitecore-nav-menu-group-list']}`}
+                  className={`${styles["sitecore-nav-menu-main-list"]} ${styles["sitecore-nav-menu-group-list"]}`}
                 >
                   <NavMenu.Item
                     value="defaultArticlesResults"
                     key="defaultArticlesResults"
-                    className={styles['sitecore-nav-menu-default-group']}
+                    className={styles["sitecore-nav-menu-default-group"]}
                   >
-                    <NavMenu.Trigger aria-hidden className={styles['sitecore-nav-menu-default-trigger']} />
+                    <NavMenu.Trigger
+                      aria-hidden
+                      className={styles["sitecore-nav-menu-default-trigger"]}
+                    />
                     <Articles articles={articles} onItemClick={onItemClick} />
                   </NavMenu.Item>
                 </NavMenu.List>
@@ -140,5 +159,9 @@ export const PreviewSearchComponent = ({ defaultItemsPerPage = 6 }) => {
     </NavMenu.Root>
   );
 };
-const PreviewSearchWidget = widget(PreviewSearchComponent, WidgetDataType.PREVIEW_SEARCH, 'content');
+const PreviewSearchWidget = widget(
+  PreviewSearchComponent,
+  WidgetDataType.PREVIEW_SEARCH,
+  "content"
+);
 export default PreviewSearchWidget;
